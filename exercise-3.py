e8aa438d05f1cd9eb3b6fd49adbce6a69b68e517
FareedRehan <fareed.rehan@dci-student.org>
2023-08-31 19:31:33 +0200

    exercise-3 is added

---
+++ exercise-3.py
@@ -0,0 +1,15 @@
+# Upper / lower in Python
+
+## Tasks
+
+### Original string
+text = "The Quick BroWnFoX jumps over the laZy Dog!"
+print("Original string:", text)
+
+### uppercase string
+text = "The Quick BroWnFoX jumps over the laZy Dog!"
+print("String in uppercase:", text.upper())
+
+## lowercase string
+text = "The Quick BroWnFoX jumps over the laZy Dog!"
+print("String in lowercase:", text.lower())
